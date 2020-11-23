229919337d4ccc065375aa1363e3666e57bbac0e
Jheric Quilatan <65877224+Jekkers08@users.noreply.github.com>
2020-11-23 18:38:27 -0500

    Added fade in animation to all photo files

--- photos.js
+++ photos.js
@@ -3,6 +3,7 @@ lightbox.id = 'lightbox';
 document.body.appendChild(lightbox);
 
 const images = document.querySelectorAll('img');
+
 images.forEach(image => {
   image.addEventListener('click', e => {
     lightbox.classList.add('active');
