da6d73c011dc0bb0340fd0a9a7a7b73b73f43cd2
Jheric <jhericq08@gmail.com>
2021-06-07 07:08:17 -0400

    finished main viewport of portraits.html. rearranged some photo's positiongs

--- js/portraits.js
+++ js/portraits.js
@@ -2,32 +2,32 @@ let elem = document.querySelector(".grid");
 let msnry = new Masonry(elem, {
   // options
   itemSelector: ".grid-item",
-  columnWidth: 160,
+  columnWidth: 180,
 });
 
-// const lightbox = document.createElement('div');
-// lightbox.id = 'lightbox';
+// const lightbox = document.createElement("div");
+// lightbox.id = "lightbox";
 // document.body.appendChild(lightbox);
 
-// const images = document.querySelectorAll('img:not(#logo)');
+// const images = document.querySelectorAll("img:not(#logo)");
 
-// images.forEach(image => {
-//   image.addEventListener('click', e => {
-//     lightbox.classList.add('active');
-//     const img = document.createElement('img');
+// images.forEach((image) => {
+//   image.addEventListener("click", (e) => {
+//     lightbox.classList.add("active");
+//     const img = document.createElement("img");
 //     img.src = image.src;
 //     while (lightbox.firstChild) {
 //       lightbox.removeChild(lightbox.firstChild);
 //     }
 //     lightbox.appendChild(img);
-//   })
-// })
+//   });
+// });
 
-// lightbox.addEventListener ('click', e => {
+// lightbox.addEventListener("click", (e) => {
 //   if (e.target !== e.currentTarget) return;
-//   lightbox.classList.remove('active');
-// })
+//   lightbox.classList.remove("active");
+// });
 
-// function dropDown(){
+// function dropDown() {
 //   document.getElementById("dropDownMenu").classList.toggle("show");
 // }
